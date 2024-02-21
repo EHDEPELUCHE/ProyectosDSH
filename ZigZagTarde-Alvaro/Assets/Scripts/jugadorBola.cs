4dc908b6352a6adf13e0f074aaf06b7dd361ba03
Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>
2024-02-21 19:16:44 +0100

    cosas

--- ZigZagTarde-Alvaro/Assets/Scripts/jugadorBola.cs
+++ ZigZagTarde-Alvaro/Assets/Scripts/jugadorBola.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class jugadorBola : MonoBehaviour
@@ -14,40 +15,48 @@ public class jugadorBola : MonoBehaviour
     private Vector3 DireccionActual;
     private int suelos = 0;
     private int maxSuelo = 8;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         offSet = camara.transform.position;
         CrearSueloInicial();
         DireccionActual = Vector3.forward;
-    }
+        rb = GetComponent<Rigidbody>();    }
     
     // Update is called once per frame
     void Update()
     {
         camara.transform.position = transform.position + offSet;
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space)) 
             cambiarDireccion();
+        //rb.AddTorque(DireccionActual * velocidad);
         transform.Translate(DireccionActual * velocidad * Time.deltaTime);
     }
 
     private void OnCollisionExit(Collision other) {
-        if (other.gameObject.tag == "Suelo")
+        if (other.gameObject.CompareTag("Suelo")) {
             StartCoroutine(BorrarSuelo(other.gameObject));
+            if (suelos < maxSuelo) {
+                StartCoroutine(CrearSuelo(other.gameObject));
+            }
+        }
+    }
+
+    IEnumerator CrearSuelo(GameObject suelo) {
+        if (suelos < maxSuelo) {
+            float aleatorio = Random.Range(0.0f, 1.0f);
+            if (aleatorio > 0.5) ValX += 6.0f;
+            else ValZ += 6.0f;
+            Instantiate(suelo, new Vector3(ValX, 0, ValZ), Quaternion.identity);
+            suelos++;
+        } else yield return new WaitForSeconds(0.1f);
     }
 
     IEnumerator BorrarSuelo(GameObject suelo) {
-        float aleatorio = Random.Range(0.0f, 1.0f);
-        if (aleatorio > 0.5) ValX += 6.0f;
-        else ValZ += 6.0f;
-        if (suelos < maxSuelo) {
-            Instantiate(suelo, new Vector3(ValX, 0, ValZ), Quaternion.identity);
-            suelos++;
-        }
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(1.0f);
         suelo.gameObject.GetComponent<Rigidbody>().isKinematic = false;
         suelo.gameObject.GetComponent<Rigidbody>().useGravity = false;
-        yield return new WaitForSeconds(1);
         Destroy(suelo);
         suelos--;
     }
