3258978d4402695a5ef866ba5c32b8f0227c7dc0
Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>
2024-02-23 18:28:19 +0100

    Actualización de archivos binarios en la biblioteca y correción de error en el movimiento del jugador.

---
+++ ZigzagPareja/Assets/Scripts/jugadorBola.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class jugadorBola : MonoBehaviour
+{
+    //Publicas
+    public Camera camara;
+    public GameObject suelo;
+    public float velocidad = 10.0f; 
+    //Privadas
+    private Vector3 offSet;
+    private float ValX, ValZ;
+    private Vector3 DireccionActual;
+    private int suelos = 0;
+    private int maxSuelo = 8;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offSet = camara.transform.position;
+        CrearSueloInicial();
+        DireccionActual = Vector3.forward;
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) 
+            cambiarDireccion();
+    }
+
+    void FixedUpdate()
+    {
+        camara.transform.position = transform.position + offSet;
+        rb.velocity = DireccionActual * velocidad;
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.CompareTag("Suelo")) {
+            StartCoroutine(BorrarSuelo(other.gameObject));
+            if (suelos < maxSuelo) {
+                StartCoroutine(CrearSuelo(other.gameObject));
+            }
+        }
+    }
+
+    IEnumerator CrearSuelo(GameObject suelo) {
+        if (suelos < maxSuelo) {
+            float aleatorio = Random.Range(0.0f, 1.0f);
+            if (aleatorio > 0.5) ValX += 6.0f;
+            else ValZ += 6.0f;
+            Instantiate(suelo, new Vector3(ValX, 0, ValZ), Quaternion.identity);
+            suelos++;
+        } else yield return new WaitForSeconds(0.1f);
+    }
+
+    IEnumerator BorrarSuelo(GameObject suelo) {
+        yield return new WaitForSeconds(1.0f);
+        suelo.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        suelo.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        Destroy(suelo);
+        suelos--;
+    }
+
+    void cambiarDireccion() {
+        if (DireccionActual == Vector3.forward)
+            DireccionActual = Vector3.right;
+        else
+            DireccionActual = Vector3.forward;
+    }
+
+    void CrearSueloInicial() {
+        for (int j = 0; j < 3; j++) {
+            ValZ += 6.0f;
+            Instantiate(suelo, new Vector3(ValX, 0, ValZ), Quaternion.identity);
+            suelos++;
+        }
+    }
+}
