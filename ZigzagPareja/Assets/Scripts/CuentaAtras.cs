e4247229d5fe78633cb690644571993c2c39fffc
elenavaz <elena.vazrodri@alum.uca.es>
2024-02-23 15:20:07 +0100

    menu inicial funcional

---
+++ ZigzagPareja/Assets/Scripts/CuentaAtras.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CuentaAtras : MonoBehaviour
+{
+    //Publica
+    private Button botonEmpezar;
+    private Button botonSalir;
+    public Image imagen;
+    public Sprite[] numeros;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        botonEmpezar = GameObject.FindWithTag("EmpezarBoton").GetComponent<Button>();
+        botonSalir = GameObject.FindWithTag("Salir").GetComponent<Button>();
+        botonEmpezar.onClick.AddListener(Empezar);
+        botonSalir.onClick.AddListener(Terminar);
+
+    }
+
+    void Empezar(){
+        imagen.gameObject.SetActive(true);
+        botonEmpezar.gameObject.SetActive(false);
+        botonSalir.gameObject.SetActive(false);
+        StartCoroutine(cuentaAtras());
+    }
+
+    void Terminar(){
+        Debug.Log("Cerrando");
+        Application.Quit();
+    }
+    IEnumerator cuentaAtras(){
+        for(int i = 2;i >= 0; i--){
+            imagen.sprite = numeros[i];
+            yield return new WaitForSeconds(1);
+        }
+        SceneManager.LoadScene("Nivel1");
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
